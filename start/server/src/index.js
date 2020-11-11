fb83ddf79a891202ff69fd16a1340bc282c41bc1
Slava <ucheba00@gmail.com>
2020-11-11 00:07:02 -0700

    build a schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
